2d2c8a88f674ec20b7d370b8c5aef5bf2f4979f5
otm099 <otahiri077@gmail.com>
2026-03-21 20:10:56 +0100

    first commit

---
+++ src/main/java/BackEnd_Library/demo/entity/Borrow.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BackEnd_Library.demo.entity;
+
+/**
+ *
+ * @author USER
+ */
+public class Borrow {
+    
+}
