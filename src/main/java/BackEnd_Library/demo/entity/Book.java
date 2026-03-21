6d6494304790aea948a7e0720222acd6fccbc758
otm099 <otahiri077@gmail.com>
2026-03-21 22:04:06 +0100

    first commit

--- src/main/java/BackEnd_Library/demo/entity/Book.java
+++ src/main/java/BackEnd_Library/demo/entity/Book.java
@@ -4,10 +4,52 @@
  */
 package BackEnd_Library.demo.entity;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+
 /**
  *
  * @author USER
  */
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    private String title;
+    private String author;
+    private String category;
+    private int quantity;
+    
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    private List<Borrow> borrows ;
+    
+    public Book(){};
+    
+    public Book(String title, String author, String category, int quantity){
+        this.title = title;
+        this.author = author;
+        this.category = category ;
+        this.quantity = quantity ;
+    }
+    
+    public Long getId(){ return id ;}
+    
+    public String getTitle(){ return title ; }
+    public void setTitle(String title){ this.title = title ; }
+    
+    public String getAuthor(){ return author ; }
+    public void setAuthor(String author) { this.author= author ; }
+    
+    public String getCategory(){ return category ; }
+    public void setCategory(String category) { this.category = category ; }
+    
+    public int getQuantity(){ return quantity ; }
+    public void setQuantity(int quantity){ this.quantity = quantity ;}
+    
+    public List<Borrow> getBorrows(){ return borrows ; }
+    public void setBorrows(List <Borrow> borrows){ this.borrows = borrows ; }
 }
--- src/main/java/BackEnd_Library/demo/entity/User.java
+++ src/main/java/BackEnd_Library/demo/entity/User.java
@@ -4,10 +4,53 @@
  */
 package BackEnd_Library.demo.entity;
 
+import jakarta.persistence.*;
+import java.util.List;
 /**
  *
  * @author USER
  */
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ; 
+    
+    private String name ;
+    
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+    
+    @OneToMany(mappedBy="usesr", cascade = CascadeType.ALL)
+    private List<Borrow> borrows ;
+    
+    public User(){}
+    
+    public User(String name, String email, String password, String role){
+        this.name=name;
+        this.email = email;
+        this.password = password ; 
+        this.role = role ;
+    }
+    
+    public Long getID(){ return id ; }
+    
+    public String getName(){ return name ; }
+    public void setName(String name ){ this.name = name ;}
+    
+    public String getEmail() { return email ; }
+    public void setEmail(String email){ this.email = email ; }
+    
+    public String getPassword(){ return password ; }
+    public void setPassword(String password){ this.password = password ; }
+    
+    public String getRole(){return role ; }
+    public void setRole(String role){ this.role = role ; }
+    
+    public List<Borrow> getBorrow(){ return borrows ; }
+    public void setBorrow(List<Borrow> borrows){this.borrows=borrows;}
     
 }
